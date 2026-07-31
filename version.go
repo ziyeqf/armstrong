5705f429705d63b3ff613a1af09d6e16d17117bc
teowa <104055472+teowa@users.noreply.github.com>
2026-07-31 09:57:13 +0000

    fix releaser

--- version.go
+++ version.go
@@ -1,5 +1,8 @@
 package main
 
+// version is set from the release tag by GoReleaser in the release pipeline.
+var version = "dev"
+
 func VersionString() string {
-	return "0.17.0"
+	return version
 }
